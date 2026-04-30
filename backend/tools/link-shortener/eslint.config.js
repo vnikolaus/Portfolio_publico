00edddb3dfb9f0e846edbab0095d94767614f211
vnikolaus <victor_nikolaus@hotmail.com>
2026-04-30 17:13:01 -0300

    commit port_publico

---
+++ backend/tools/link-shortener/eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+    {
+        ignores: ['dist/**', 'node_modules/**']
+    },
+    js.configs.recommended,
+    ...tsPlugin.configs['flat/recommended'],
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType:  'module',
+            globals: {
+                console: 'readonly',
+                process: 'readonly'
+            }
+        },
+        rules: {
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/no-explicit-any': 'warn',
+            'no-console': 'off'
+        }
+    }
+];

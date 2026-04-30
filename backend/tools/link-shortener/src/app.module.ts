1e72145eb3b2fd96fa6662d7b19cb9c8d4d01719
vnikolaus <victor_nikolaus@hotmail.com>
2026-04-30 17:58:29 -0300

    commit port_publico

--- backend/tools/link-shortener/src/app.module.ts
+++ backend/tools/link-shortener/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { DatabaseModule } from './database/database.module';
-import { LinksModule } from './links/links.module';
+import { LinksModule } from './app/modules/links/links.module';
+import { DatabaseModule } from './infra/database/database.module';
 
 @Module({
     imports: [ConfigModule.forRoot({ isGlobal: true }), LinksModule, DatabaseModule],
